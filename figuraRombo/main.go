3161eefcd3a2f8446fe5d357cd2bc4eb6da8605f
juanbnunez <contact@juanbnunez.info>
2023-08-10 17:09:49 -0600

    Ejercicio rombo

--- figuraRombo/main.go
+++ figuraRombo/main.go
@@ -1,18 +1,35 @@
 package main
 
-func Rombo(lineaCentroSize int) {
-	if lineaCentroSize%2 == 0 {
+import (
+	"fmt"
+)
+
+func Rombo(n int) {
+	if n%2 == 0 {
 		println("La linea central debe tener una cantidad impar de asteriscos")
 	} else {
-		for i := 0; i <= lineaCentroSize; i++ {
-			for j := lineaCentroSize; j >= 0; j-- {
-				print("*")
+		for i := 0; i < n; i++ {
+			for j := 0; j < n-i-1; j++ {
+				fmt.Print(" ")
 			}
-			println()
+			for j := 0; j < 2*i+1; j++ {
+				fmt.Print("*")
+			}
+			fmt.Println()
+		}
+
+		for i := n - 2; i >= 0; i-- {
+			for j := 0; j < n-i-1; j++ {
+				fmt.Print(" ")
+			}
+			for j := 0; j < 2*i+1; j++ {
+				fmt.Print("*")
+			}
+			fmt.Println()
 		}
 	}
 }
 
 func main() {
-	Rombo(5)
+	Rombo(9)
 }
