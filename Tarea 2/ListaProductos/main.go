7ae77ccf0cf167a4b6b41faf7de72f4e455aeaa4
juanbnunez <contact@juanbnunez.info>
2023-08-20 15:45:13 -0600

    First part and translation to english
    The code was modify to be in english and added the first part of the homework (minimum existence)

--- Tarea 2/ListaProductos/main.go
+++ Tarea 2/ListaProductos/main.go
@@ -13,40 +13,69 @@ var lProductos listaProductos
 
 const existenciaMinima int = 10 //la existencia mínima es el número mínimo debajo de el cual se deben tomar eventuales desiciones
 
+// -----------------------------método de agregar producto con puntero hacia la listaProductos-----------------------------
 func (l *listaProductos) agregarProducto(nombre string, cantidad int, precio int) {
-	*l = append(*l, producto{nombre: nombre, cantidad: cantidad, precio: precio})
+	bandera := false
+	for n := 0; n < len(lProductos); n++ {
+		if nombre == (*l)[n].nombre {
+			(*l)[n].cantidad += cantidad
+			bandera = true
+			if precio != (*l)[n].precio {
+				(*l)[n].precio = precio
+			}
+		}
+	}
+	if !bandera {
+		*l = append(*l, producto{nombre: nombre, cantidad: cantidad, precio: precio})
+	}
 	// modificar el código para que cuando se agregue un producto, si este ya se encuentra, incrementar la cantidad
 	// de elementos del producto y eventualmente el precio si es que es diferente
 }
 
 // Hacer una función adicional que reciba una cantidad potencialmente infinita de productos previamente creados y los agregue a la lista
+func (l *listaProductos) incrementarProducto(nombre string, cantidad int) {
+	for i := 0; i < len(*l); i++ {
+		if (*l)[i].nombre == nombre {
+			(*l)[i].cantidad += cantidad
+		}
+	}
+}
 
-func (l *listaProductos) buscarProducto(nombre string) (producto, int) { //el retorno es el índice del producto encontrado y -1 si no existe
-	var productoEncontrado producto
-	var err int = 0
+/*
+func (l *listaProductos) buscarProducto(nombre string) int { //el retorno es el índice del producto encontrado y -1 si no existe
+	var result = -1
 	var i int
 	for i = 0; i < len(*l); i++ {
 		if (*l)[i].nombre == nombre {
-			productoEncontrado = (*l)[i]
-			return productoEncontrado, err
+			result = i
 		}
 	}
-	err = -1
-	return productoEncontrado, err
+	return result
+}*/
+
+// modifique la función para que esta vez solo retorne el producto como tal y que retorne otro valor adicional "err" conteniendo un 0 si no hay error y números mayores si existe algún
+// tipo de error como por ejemplo que el producto no exista. Una vez implementada la nueva función, cambie los usos de la anterior función en funciones posteriores, realizando los cambios
+// que el uso de la nueva función ameriten
+func (l *listaProductos) buscarProducto(nombre string) (*producto, int) { //el retorno es el índice del producto encontrado y -1 si no existe
+	//no hacer un retorno de una copia, sino un retorno de un cursor que apunte hacia la posición del producto
+	//así si se ocupa modificar se modifica la posición y no una copia
+	var err = -1
+	var p *producto = nil
+	for i := 0; i < len(*l); i++ {
+		if (*l)[i].nombre == nombre {
+			//& asigna la dirección
+			p = &((*l)[i])
+			err = 0
+		}
+	}
+	//retorna la dirección del producto y si existe error o no
+	return p, err
 }
 
-//REALIZADO
-//para la funcion buscarProducto
-//modifique la función para que esta vez solo retorne el producto como tal y que retorne otro valor adicional "err"
-//conteniendo un 0 si no hay error y números mayores si existe algún
-//tipo de error como por ejemplo que el producto no exista. Una vez implementada la nueva función, cambie los usos de la
-//anterior función en funciones posteriores, realizando los cambios
-//que el uso de la nueva función ameriten
-
 func (l *listaProductos) venderProducto(nombre string) {
-	var prod = l.buscarProducto(nombre)
-	if prod != -1 {
-		(*l)[prod].cantidad = (*l)[prod].cantidad - 1
+	var prod, err = l.buscarProducto(nombre)
+	if err != -1 {
+		(*prod).cantidad = (*prod).cantidad - 1
 		//modificar para que cuando no haya existencia de cantidad de productos, el producto se elimine de "la lista" y notificar dicha acción
 		//modifique posteriormente para que se ingrese de parámetro la cantidad de productos a vender
 	}
@@ -59,6 +88,8 @@ func llenarDatos() {
 	lProductos.agregarProducto("frijoles", 4, 2000)
 	lProductos.agregarProducto("leche", 8, 1200)
 	lProductos.agregarProducto("café", 12, 4500)
+	lProductos.agregarProducto("café", 12, 5000)
+	lProductos.incrementarProducto("arroz", 30)
 
 }
 
@@ -71,4 +102,7 @@ func main() {
 	llenarDatos()
 	fmt.Println(lProductos)
 	//venda productos
+	//lProductos.venderProducto("arroz")
+	//lProductos.venderProducto("frijoles")
+	//fmt.Println("Lista actualizada: ", lProductos)
 }
--- Tarea 2/ListaProductosTarea/main.go
+++ Tarea 2/ListaProductosTarea/main.go
@@ -1,96 +1,136 @@
-package ListaProductosTarea
+package main
 
 import "fmt"
 
-type producto struct {
-	nombre   string
-	cantidad int
-	precio   int
-}
-type listaProductos []producto
-
-var lProductos listaProductos
-
-const existenciaMinima int = 10 //la existencia mínima es el número mínimo debajo de el cual se deben tomar eventuales desiciones
-
-func (l *listaProductos) agregarProducto(nombre string, cantidad int, precio int) {
-	*l = append(*l, producto{nombre: nombre, cantidad: cantidad, precio: precio})
-	// modificar el código para que cuando se agregue un producto, si este ya se encuentra, incrementar la cantidad
-	// de elementos del producto y eventualmente el precio si es que es diferente
+// Struct for representing a product
+type product struct {
+	name     string
+	quantity int
+	price    int
 }
 
-// Hacer una función adicional que reciba una cantidad potencialmente infinita de productos previamente creados y los agregue a la lista
+// A collection of products
+type productInventory []product
 
-func (l *listaProductos) buscarProducto(nombre string) (producto, int) { //el retorno es el índice del producto encontrado y -1 si no existe
-	var productoEncontrado producto
-	var err int = 0
-	var i int
-	for i = 0; i < len(*l); i++ {
-		if (*l)[i].nombre == nombre {
-			productoEncontrado = (*l)[i]
-			return productoEncontrado, err
-		}
-	}
-	err = -1
-	return productoEncontrado, err
-}
+var products productInventory
+var lowStockProducts productInventory
 
-//REALIZADO
-//para la funcion buscarProducto
-//modifique la función para que esta vez solo retorne el producto como tal y que retorne otro valor adicional "err"
-//conteniendo un 0 si no hay error y números mayores si existe algún
-//tipo de error como por ejemplo que el producto no exista. Una vez implementada la nueva función, cambie los usos de la
-//anterior función en funciones posteriores, realizando los cambios
-//que el uso de la nueva función ameriten
+const minimumStock int = 10 // The minimum stock level triggering decisions
 
-func (l *listaProductos) venderProducto(nombre string, cantVenta int) {
-	prod, index := l.buscarProducto(nombre)
-	if prod != nil {
-		if prod.cantidad >= cantVenta {
-			prod.cantidad -= cantVenta
-
-			if prod.cantidad == 0 {
-				// Eliminar el producto de la lista
-				copy(l.productos[index:], l.productos[index+1:])
-				l.productos = l.productos[:len(l.productos)-1]
-				fmt.Printf("Producto '%s' se agotó y fue eliminado de la lista.\n", prod.nombre)
-			} else {
-				fmt.Printf("Venta de %d unidades de '%s' realizada con éxito.\n", cantVenta, prod.nombre)
+// AddProduct adds a product to the inventory or updates its details
+func (p *productInventory) AddProduct(name string, quantity int, price int) {
+	found := false
+	for n := 0; n < len(products); n++ {
+		if name == (*p)[n].name {
+			(*p)[n].quantity += quantity
+			found = true
+			if price != (*p)[n].price {
+				(*p)[n].price = price
 			}
-		} else {
-			fmt.Println("Cantidad insuficiente para realizar la venta")
 		}
-	} else {
-		fmt.Println("Producto no encontrado")
+	}
+	if !found {
+		*p = append(*p, product{name: name, quantity: quantity, price: price})
 	}
 }
-En este código, he implementado la eliminación del producto de la lista utilizando la función copy para eliminar el producto en la posición index y reorganizar los elementos restantes en el slice. Además, he agregado mensajes de notificación para cuando se realiza una venta exitosa o cuando un producto se agota y se elimina de la lista.
 
-Recuerda que este código es un ejemplo simplificado y que podría necesitar ajustes según la estructura y requerimientos específicos de tu programa.
-
-
-
-
-
-
-
-//haga una función para a partir del nombre del producto, se pueda modificar el precio del mismo Pero utilizando la función buscarProducto MODIFICADA PREVIAMENTE
-
-func llenarDatos() {
-	lProductos.agregarProducto("arroz", 15, 2500)
-	lProductos.agregarProducto("frijoles", 4, 2000)
-	lProductos.agregarProducto("leche", 8, 1200)
-	lProductos.agregarProducto("café", 12, 4500)
-
+// IncrementProduct increases the quantity of an existing product
+func (p *productInventory) IncrementProduct(name string, quantity int) {
+	for i := 0; i < len(*p); i++ {
+		if (*p)[i].name == name {
+			(*p)[i].quantity += quantity
+		}
+	}
 }
 
-func (l *listaProductos) listarProductosMínimos() listaProductos {
-	// debe retornar una nueva lista con productos con existencia mínima
-	return nil
+// FindProduct searches for a product by name and returns a pointer to it and an error status
+func (p *productInventory) FindProduct(name string) (*product, int) {
+	var err = -1
+	var prod *product = nil
+	for i := 0; i < len(*p); i++ {
+		if (*p)[i].name == name {
+			prod = &((*p)[i])
+			err = 0
+		}
+	}
+	return prod, err
+}
+
+// SellProduct handles the process of selling a product
+func (p *productInventory) SellProduct(name string, quantity int) {
+	prod, err := p.FindProduct(name)
+	if err != -1 {
+		if (*prod).quantity >= quantity {
+			(*prod).quantity -= quantity
+		}
+		if (*prod).quantity == 0 {
+			fmt.Printf("The product '%s' has been depleted and removed from the list.\n", prod.name)
+			p.DeleteProduct(prod)
+		} else {
+			fmt.Println("Insufficient quantity for the sale")
+		}
+	}
+}
+
+// DeleteProduct removes a product from the inventory
+func (p *productInventory) DeleteProduct(prod *product) {
+	for i, pr := range *p {
+		if pr.name == prod.name {
+			*p = append((*p)[:i], (*p)[i+1:]...)
+			break
+		}
+	}
+}
+
+// IncrementPrice updates the price of a product
+func (p *productInventory) IncrementPrice(name string, price int) {
+	prod, err := p.FindProduct(name)
+	if err != -1 {
+		(*prod).price = price
+		fmt.Println("Price updated")
+	}
+}
+
+// GetLowStockList retrieves a list of products with low stock
+func (p *productInventory) GetLowStockList() productInventory {
+	for _, pr := range *p {
+		if pr.quantity <= minimumStock {
+			lowStockProducts = append(lowStockProducts, pr)
+		}
+	}
+	return lowStockProducts
+}
+
+// ReplenishLowStockInventory replenishes the inventory for products with low stock
+func (p *productInventory) ReplenishLowStockInventory(minimumStockList productInventory) {
+	for i := 0; i < len(minimumStockList); i++ {
+		minimumStockList[i].quantity = minimumStock
+	}
+}
+
+// FillData initializes the product inventory with sample data
+func FillData() {
+	products.AddProduct("rice", 15, 2500)
+	products.AddProduct("beans", 4, 2000)
+	products.AddProduct("milk", 8, 1200)
+	products.AddProduct("coffee", 12, 4500)
+	products.AddProduct("coffee", 12, 5000)
 }
 
 func main() {
-	llenarDatos()
-	fmt.Println(lProductos)
-	//venda productos
+	FillData()
+
+	fmt.Println("Products:", products)
+
+	// Sell products
+	products.SellProduct("rice", 15)
+	fmt.Println("Updated list:", products)
+
+	// List low stock products
+	products.GetLowStockList()
+	fmt.Println("Low stock list:", lowStockProducts)
+
+	// Replenish inventory
+	products.ReplenishLowStockInventory(lowStockProducts)
+	fmt.Println("Updated list:", lowStockProducts)
 }
